31d6b830bf9faacdf3c429c76f4d740b5405cd23
Ania <candyrockstore5@gmail.com>
2021-09-21 10:41:23 +0200

    Update server

--- src/server/index.js
+++ src/server/index.js
@@ -1,23 +1,50 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 var path = require("path");
 const express = require("express");
+const bodyParser = require("body-parser");
+const fetch = require("node-fetch");
 const mockAPIResponse = require("./mockAPI.js");
 
 const app = express();
 
+const cors = require("cors");
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(express.static("dist"));
 
 console.log(__dirname);
 
+const baseURL = "https://api.meaningcloud.com/sentiment-2.1?";
+const apiKey = process.env.API_KEY;
+console.log(`Your API Key is ${process.env.API_KEY}`);
+let userInput = [];
+
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  res.sendFile(path.resolve("src/client/views/index.html"));
+  res.sendFile("dist/index.html");
+  //res.sendFile(path.resolve('src/client/views/index.html'))
+});
+
+app.get("/test", function (req, res) {
+  res.send(mockAPIResponse);
+});
+
+app.post("/api", async function (req, res) {
+  userInput = req.body.url;
+  console.log(`You entered: ${userInput}`);
+  const apiURL = `${baseURL}key=${apiKey}&url=${userInput}&lang=en`;
+
+  const response = await fetch(apiURL);
+  const mcData = await response.json();
+  console.log(mcData);
+  res.send(mcData);
 });
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
   console.log("Example app listening on port 8080!");
-});
-
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
 });
